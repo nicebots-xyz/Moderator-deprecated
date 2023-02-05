3ebc95574663bd0006bfe6da039c0741881d62b4
Paillat <git@electronique.cc>
2023-02-05 22:38:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -165,10 +165,6 @@ async def on_message( message: discord.Message):
     #if the content starts with everything excpet something containing a space and then ! , we don't want to moderate it because it is a command
     #first we check if the content starts with everything and then !
     #with regex, we check if the content starts with anything and then !
-    if re.match(r".*!", content):
-        #if the match dosent contain a space, we don't want to moderate it because it is a command
-        if not " " in re.match(r".*!", content).group(0):
-            return
     #we also do that with the manage_messages permission, so the moderators can't be moderated
     if not content.startswith("MOD TEST"):
         if message.author.guild_permissions.manage_messages: return #if the user is a moderator, we don't want to moderate him because he is allowed to say whatever he wants because he is just like a dictator
@@ -176,6 +172,10 @@ async def on_message( message: discord.Message):
     else:
         content = content.replace("MOD TEST ", "")
         content = content.replace("MOD TEST", "")
+    if re.match(r".*!", content):
+    #if the match dosent contain a space, we don't want to moderate it because it is a command
+        if not " " in re.match(r".*!", content).group(0):
+            return
     if not is_enabled: return
     message_toxicity = tox.get_toxicity(content)
     reasons_to_delete = []
